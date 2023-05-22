8a06f0776dcf567e5a5888802bf37957af4bd6a9
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-05-22 13:27:45 -0600

    a little closer

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -20,7 +20,7 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log("Invalid token line 24");
+      console.log("Invalid token line 23");
       console.log(token);
     }
 
